c68ec12188626a47b2c5c9df04e153bc9ff151e2
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2025-01-15 18:37:30 +1100

    remove, length, clear, keys functions written

--- script.js
+++ script.js
@@ -18,6 +18,7 @@
         return hashCode;
     }
 
+    //takes two arguments: the first is a key, and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten, and we can say that we update the key’s value (e.g. Carlos is our key but it is called twice: once with value I am the old value., and once with value I am the new value.. Following this logic, Carlos should contain only the latter value).
     set(key, value){
         //if the number of entries in the hashmap exceed our loadfactor of the total capacity of our hashmap
         //double the capacity and re-hash current entries into new array
@@ -89,11 +90,15 @@
             }
             this.size++; 
 
-            console.log(`Index: ${index}, Key: ${this.table[index].key}, Value: ${this.table[index].value}, Size: ${this.size}`);
+            for(const bucket of this.table){
+                if(!bucket === undefined){
+                    console.log(bucket.toString());
+                }
+            }
         }
     }
 
-
+    //takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
     get(key) {
         const index = this.hash(key);
 
@@ -112,6 +117,7 @@
         return value;
     }
 
+    //takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
     has(key) {
         const index = this.hash(key);
 
@@ -120,17 +126,6 @@
         }
 
         if(this.table[index]){
-            // if(this.table[index].key === key) {
-            //     console.log('key exists');
-            //     return true;
-            // } else {
-            //     console.log('new key entry');
-            //     return false;
-            // }
-
-            //Go through each node of the linkedList and check if the key exists already
-            //If there is a matching key return true
-            //Else return false
 
             const linkedList = this.table[index];
             if(linkedList.contains(key)) {
@@ -140,6 +135,44 @@
             }
         }
     }
+
+    // takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return true. If the key isn’t in the hash map, it should return false.
+    remove(key) {
+        const index = this.hash(key);
+
+        if (index < 0 || index >= this.table.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+
+        if(this.table[index]){
+            if(this.table[index].contains(key)){
+                let depthOfKey = this.table[index].find(key);
+
+                this.table[index].removeAt(depthOfKey);
+                this.size--;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    //returns the number of stored keys in the hash map.
+    length() {
+        console.log(`total keys in hash map: ${this.size}`);
+        return this.size;
+    }
+
+    //removes all entries in the hash map.
+    clear() {
+        this.table = new Array(this.capacity);
+    }
+
+    //returns an array containing all the values.
+    values() {
+        
+    }
+
   }
 
 class Node {
@@ -211,8 +244,28 @@ class LinkedList {
             current = current.nextNode;
         }
 
-        console.log("NO MATCHING KEY WAS FOUND!! \n ")
         return false; 
+    }
+
+    //find index of key in node tree
+
+    find(key){
+        let current = this.head;
+        let count = 0;
+
+        while(current.nextNode){
+            if(current.key === key){
+                console.log(`KEY FOUND AT INDEX:`);
+                console.log(`${count} \n`);
+                return count;
+            }
+
+            current = current.nextNode;
+            count++;
+        }
+
+        console.log(`VALUE NOT FOUND AT ANY INDEX :(((`);
+        return null;
     }
 
     retrieveValueFromKey(key, value) {
@@ -228,6 +281,44 @@ class LinkedList {
 
         console.log("NO MATCHING KEY WAS FOUND!! \n ")
         return false; 
+    }
+
+    //Remove node at paticular index
+
+    removeAt(index) {
+        //if index is out of range 
+        if(index > 0 && index > this.size) {
+            return;
+        }
+
+        let count = 0;
+        let current;
+        let previous;
+
+        //if list is empty
+        if(!this.head) {
+            //make head
+            this.head = node;
+            return;
+        }   else {
+            //start at beginning of list
+            previous = null;
+            current = this.head;
+            
+            //cycle through list until the correct index node
+            while(count < index) {
+                //node before index
+                previous = current; 
+                count++;
+                //node after index
+                current = current.nextNode;
+            }
+
+            //delete node at index  
+            previous.nextNode = current.nextNode;
+        }
+         //decrease tally of linked list size
+        this.size--;
     }
 
     toString(){
